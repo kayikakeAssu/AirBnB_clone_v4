3afaddb20de9d6bfecd192db47eee42bb68df3fc
kayikakeAssu <kayassurance@gmail.com>
2024-06-02 04:51:59 -0700

    refactor: hbnb files

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -19,20 +19,30 @@ def close_db(error):
             storage.close()
 
 
-            @app.route('/0-hbnb/', strict_slashes=False)
-            def hbnb():
-                    """ HBNB is alive! """
-                        states = storage.all(State).values()
-                            states = sorted(states, key=lambda k: k.name)
-                                st_ct = []
+@app.route('/0-hbnb/', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
 
-                                    for state in states:
-                                                st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+    for state in states:
+            st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
 
-                                                    amenities = storage.all(Amenity).values()
-                                                        amenities = sorted(amenities, key=lambda k: k.name)
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
 
-                                                            places = storage.all(Place).values()
-                                                                places = sorted(places, key=lambda k: k.name)
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
 
-                                                                    return render_template('0-hbnb.html',
+    return render_template('0-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=uuid.uuid4())
+
+
+if __name__ == "__main__":
+            """ Main Function """
+                app.run(host='0.0.0.0', port=5001)
+                
--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -19,27 +19,27 @@ def close_db(error):
             storage.close()
 
 
-            @app.route('/2-hbnb/', strict_slashes=False)
-            def hbnb():
-                    """ HBNB is alive! """
-                        states = storage.all(State).values()
-                            states = sorted(states, key=lambda k: k.name)
-                                st_ct = []
+@app.route('/2-hbnb/', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
 
-                                    for state in states:
-                                                st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+    for state in states:
+            st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
 
-                                                    amenities = storage.all(Amenity).values()
-                                                        amenities = sorted(amenities, key=lambda k: k.name)
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
 
-                                                            places = storage.all(Place).values()
-                                                                places = sorted(places, key=lambda k: k.name)
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
 
-                                                                    return render_template('0-hbnb.html',
-                                                                                           states=st_ct,
-                                                                                           amenities=amenities,
-                                                                                           places=places,
-                                                                                           cache_id=uuid.uuid4())
+    return render_template('0-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=uuid.uuid4())
 
 if __name__ == "__main__":
         """ Main Function """
